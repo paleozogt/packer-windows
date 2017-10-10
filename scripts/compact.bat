d204337c741d9147be1f6c38d5bae6de8ade8fc7
Aaron Simmons <paleozogt@gmail.com>
2017-10-10 14:57:50 -0600

    udefrag checks for 32/64-bit

--- scripts/compact.bat
+++ scripts/compact.bat
@@ -8,10 +8,16 @@ if not exist "C:\Windows\Temp\7z.msi" (
 msiexec /qb /i C:\Windows\Temp\7z.msi
 
 if not exist "C:\Windows\Temp\ultradefrag.zip" (
-	powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://downloads.sourceforge.net/project/ultradefrag/stable-release/6.1.0/ultradefrag-portable-6.1.0.bin.amd64.zip', 'C:\Windows\Temp\ultradefrag.zip')" <NUL
+    if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+        set UDEFRAG_DIR="C:\Windows\Temp\ultradefrag-portable-6.1.0.amd64"
+        powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://downloads.sourceforge.net/project/ultradefrag/stable-release/6.1.0/ultradefrag-portable-6.1.0.bin.amd64.zip', 'C:\Windows\Temp\ultradefrag.zip')" <NUL
+    ) else (
+        set UDEFRAG_DIR="C:\Windows\Temp\ultradefrag-portable-6.1.0.i386"
+        powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://downloads.sourceforge.net/project/ultradefrag/stable-release/6.1.0/ultradefrag-portable-6.1.0.bin.i386.zip' , 'C:\Windows\Temp\ultradefrag.zip')" <NUL
+    )
 )
 
-if not exist "C:\Windows\Temp\ultradefrag-portable-6.1.0.amd64\udefrag.exe" (
+if not exist "%UDEFRAG_DIR%\udefrag.exe" (
 	cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\ultradefrag.zip -oC:\Windows\Temp"
 )
 
@@ -30,7 +36,7 @@ rmdir /S /Q C:\Windows\SoftwareDistribution\Download
 mkdir C:\Windows\SoftwareDistribution\Download
 net start wuauserv
 
-cmd /c C:\Windows\Temp\ultradefrag-portable-6.1.0.amd64\udefrag.exe --optimize --repeat C:
+cmd /c %UDEFRAG_DIR%\udefrag.exe --optimize --repeat C:
 
 cmd /c %SystemRoot%\System32\reg.exe ADD HKCU\Software\Sysinternals\SDelete /v EulaAccepted /t REG_DWORD /d 1 /f
 cmd /c C:\Windows\Temp\sdelete.exe -q -z C:
